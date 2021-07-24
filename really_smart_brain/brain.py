1bde26f2ed035a98ce030a59f0b6356ce82e32aa
Victoria Zhou <victoria.juan.zhou@outlook.com>
2021-07-24 06:33:34 +0100

    image prediction

---
+++ really_smart_brain/brain.py
@@ -0,0 +1,12 @@
+from imageai.Classification import ImageClassification
+import os
+execution_path=os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
